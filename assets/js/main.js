9d9ba775343f4d31ca6112d0d01385002f187ff9
spondonit3 <spn3@spondontit.com>
2022-12-17 17:03:41 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,5 +226,18 @@
 			jQuery(this).siblings('.wizard-form-error').slideUp("3000");
 		}
 	});
-  
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
   }());
